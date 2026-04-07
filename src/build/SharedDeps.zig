24b3d267290b9a068eafe7e98c5b4243e8b9c287
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-04-07 23:12:57 +0900

    Position IME composition window at terminal cursor

--- src/build/SharedDeps.zig
+++ src/build/SharedDeps.zig
@@ -609,6 +609,7 @@ pub fn add(
                 step.linkSystemLibrary2("user32", .{});
                 step.linkSystemLibrary2("gdi32", .{});
                 step.linkSystemLibrary2("opengl32", .{});
+                step.linkSystemLibrary2("imm32", .{});
             },
         }
     }
