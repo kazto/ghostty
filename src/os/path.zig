Codavo/ghostinthewsl

windows: resolve bare commands via PATH

src/os/path.zig
@@ -9,7 +9,9 @@ const testing = std.testing;
 pub fn expand(alloc: Allocator, cmd: []const u8) !?[]u8 {
     // If the command already contains a slash, then we return it as-is
     // because it is assumed to be absolute or relative.
-    if (std.mem.indexOfScalar(u8, cmd, '/') != null) {
+    if (std.mem.indexOfScalar(u8, cmd, '/') != null or
+        std.mem.indexOfScalar(u8, cmd, '\\') != null)
+    {
         return try alloc.dupe(u8, cmd);
     }
 
@@ -23,39 +25,26 @@ pub fn expand(alloc: Allocator, cmd: []const u8) !?[]u8 {
     };
     defer if (builtin.os.tag == .windows) alloc.free(PATH);
 
-    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
+    const path_ext: []const u8 = if (builtin.os.tag == .windows)
+        (std.process.getEnvVarOwned(alloc, "PATHEXT") catch ".COM;.EXE;.BAT;.CMD")
+    else
+        "";
+    defer if (builtin.os.tag == .windows and path_ext.ptr != ".COM;.EXE;.BAT;.CMD".ptr) alloc.free(path_ext);
+
     var it = std.mem.tokenizeScalar(u8, PATH, std.fs.path.delimiter);
     var seen_eacces = false;
     while (it.next()) |search_path| {
-        // We need enough space in our path buffer to store this
-        const path_len = search_path.len + cmd.len + 1;
-        if (path_buf.len < path_len) return error.PathTooLong;
+        if (try expandSearchPath(alloc, search_path, cmd, null, &seen_eacces)) |result| {
+            return result;
+        }
 
-        // Copy in the full path
-        @memcpy(path_buf[0..search_path.len], search_path);
-        path_buf[search_path.len] = std.fs.path.sep;
-        @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
-        path_buf[path_len] = 0;
-        const full_path = path_buf[0..path_len :0];
-
-        // Stat it
-        const f = std.fs.cwd().openFile(
-            full_path,
-            .{},
-        ) catch |err| switch (err) {
-            error.FileNotFound => continue,
-            error.AccessDenied => {
-                // Accumulate this and return it later so we can try other
-                // paths that we have access to.
-                seen_eacces = true;
-                continue;
-            },
-            else => return err,
-        };
-        defer f.close();
-        const stat = try f.stat();
-        if (stat.kind != .directory and isExecutable(stat.mode)) {
-            return try alloc.dupe(u8, full_path);
+        if (builtin.os.tag == .windows and std.fs.path.extension(cmd).len == 0) {
+            var ext_it = std.mem.tokenizeScalar(u8, path_ext, ';');
+            while (ext_it.next()) |ext| {
+                if (try expandSearchPath(alloc, search_path, cmd, ext, &seen_eacces)) |result| {
+                    return result;
+                }
+            }
         }
     }
 
@@ -67,6 +56,44 @@ pub fn expand(alloc: Allocator, cmd: []const u8) !?[]u8 {
 fn isExecutable(mode: std.fs.File.Mode) bool {
     if (builtin.os.tag == .windows) return true;
     return mode & 0o0111 != 0;
+}
+
+fn expandSearchPath(
+    alloc: Allocator,
+    search_path: []const u8,
+    cmd: []const u8,
+    ext: ?[]const u8,
+    seen_eacces: *bool,
+) !?[]u8 {
+    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
+    const ext_ = ext orelse "";
+    const path_len = search_path.len + 1 + cmd.len + ext_.len;
+    if (path_buf.len <= path_len) return error.PathTooLong;
+
+    @memcpy(path_buf[0..search_path.len], search_path);
+    path_buf[search_path.len] = std.fs.path.sep;
+    @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
+    if (ext_.len > 0) {
+        @memcpy(path_buf[search_path.len + 1 + cmd.len ..][0..ext_.len], ext_);
+    }
+    path_buf[path_len] = 0;
+    const full_path = path_buf[0..path_len :0];
+
+    const f = std.fs.cwd().openFile(full_path, .{}) catch |err| switch (err) {
+        error.FileNotFound => return null,
+        error.AccessDenied => {
+            seen_eacces.* = true;
+            return null;
+        },
+        else => return err,
+    };
+    defer f.close();
+    const stat = try f.stat();
+    if (stat.kind != .directory and isExecutable(stat.mode)) {
+        return try alloc.dupe(u8, full_path);
+    }
+
+    return null;
 }
 
 // `uname -n` is the *nix equivalent of `hostname.exe` on Windows

kazto/ghostty

src/os/path.zig
@@ -14,46 +14,34 @@ pub fn expand(
 ) !?[]u8 {
     // If the command already contains a slash, then we return it as-is
     // because it is assumed to be absolute or relative.
-    if (std.mem.indexOfScalar(u8, cmd, '/') != null) {
+    if (std.mem.indexOfScalar(u8, cmd, '/') != null or
+        std.mem.indexOfScalar(u8, cmd, '\\') != null)
+    {
         return try alloc.dupe(u8, cmd);
     }
 
     const PATH = environ_map.get("PATH") orelse return null;
 
-    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
+    const path_ext: []const u8 = if (builtin.os.tag == .windows)
+        (std.process.getEnvVarOwned(alloc, "PATHEXT") catch ".COM;.EXE;.BAT;.CMD")
+    else
+        "";
+    defer if (builtin.os.tag == .windows and path_ext.ptr != ".COM;.EXE;.BAT;.CMD".ptr) alloc.free(path_ext);
+
     var it = std.mem.tokenizeScalar(u8, PATH, std.fs.path.delimiter);
     var seen_eacces = false;
     while (it.next()) |search_path| {
-        // We need enough space in our path buffer to store this
-        const path_len = search_path.len + cmd.len + 1;
-        if (path_buf.len < path_len) return error.PathTooLong;
+        if (try expandSearchPath(io, alloc, search_path, cmd, null, &seen_eacces)) |result| {
+            return result;
+        }
 
-        // Copy in the full path
-        @memcpy(path_buf[0..search_path.len], search_path);
-        path_buf[search_path.len] = std.fs.path.sep;
-        @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
-        path_buf[path_len] = 0;
-        const full_path = path_buf[0..path_len :0];
-
-        // Stat it
-        const f = std.Io.Dir.cwd().openFile(
-            io,
-            full_path,
-            .{},
-        ) catch |err| switch (err) {
-            error.FileNotFound => continue,
-            error.AccessDenied => {
-                // Accumulate this and return it later so we can try other
-                // paths that we have access to.
-                seen_eacces = true;
-                continue;
-            },
-            else => return err,
-        };
-        defer f.close(io);
-        const stat = try f.stat(io);
-        if (stat.kind != .directory and isExecutable(stat.permissions)) {
-            return try alloc.dupe(u8, full_path);
+        if (builtin.os.tag == .windows and std.fs.path.extension(cmd).len == 0) {
+            var ext_it = std.mem.tokenizeScalar(u8, path_ext, ';');
+            while (ext_it.next()) |ext| {
+                if (try expandSearchPath(io, alloc, search_path, cmd, ext, &seen_eacces)) |result| {
+                    return result;
+                }
+            }
         }
     }
 
@@ -72,6 +60,52 @@ fn isExecutable(perms: std.Io.File.Permissions) bool {
             };
         },
     };
+}
+
+fn expandSearchPath(
+    io: std.Io,
+    alloc: Allocator,
+    search_path: []const u8,
+    cmd: []const u8,
+    ext: ?[]const u8,
+    seen_eacces: *bool,
+) !?[]u8 {
+    var path_buf: [std.fs.max_path_bytes]u8 = undefined;
+    const ext_ = ext orelse "";
+    const path_len = search_path.len + 1 + cmd.len + ext_.len;
+    if (path_buf.len <= path_len) return error.PathTooLong;
+
+    @memcpy(path_buf[0..search_path.len], search_path);
+    path_buf[search_path.len] = std.fs.path.sep;
+    @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
+    if (ext_.len > 0) {
+        @memcpy(path_buf[search_path.len + 1 + cmd.len ..][0..ext_.len], ext_);
+    }
+    path_buf[path_len] = 0;
+    const full_path = path_buf[0..path_len :0];
+
+    // Stat it
+    const f = std.Io.Dir.cwd().openFile(
+        io,
+        full_path,
+        .{},
+    ) catch |err| switch (err) {
+        error.FileNotFound => return null,
+        error.AccessDenied => {
+            // Accumulate this and return it later so we can try other
+            // paths that we have access to.
+            seen_eacces.* = true;
+            return null;
+        },
+        else => return err,
+    };
+    defer f.close(io);
+    const stat = try f.stat(io);
+    if (stat.kind != .directory and isExecutable(stat.permissions)) {
+        return try alloc.dupe(u8, full_path);
+    }
+
+    return null;
 }
 
 // `uname -n` is the *nix equivalent of `hostname.exe` on Windows